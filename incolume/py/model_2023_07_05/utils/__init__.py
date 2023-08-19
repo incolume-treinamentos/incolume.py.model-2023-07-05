775fbafe2cb8e5b9e7ecfd008fa183e7c933f59c
Britodfbr <britodfbr@gmail.com>
2023-08-19 08:48:33 -0300

    Bumping version from 0.19.0a4 to 0.19.0a5

--- incolume/py/model_2023_07_05/utils/__init__.py
+++ incolume/py/model_2023_07_05/utils/__init__.py
@@ -1,22 +1,23 @@
+"""Module utils."""
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
-__author__ = "@britodfbr"  # pragma: no cover
+__author__ = '@britodfbr'  # pragma: no cover
 
 from collections import namedtuple
 
-type_color = namedtuple('Color', 'none red green blue yellow')
+TypeColor = namedtuple('TypeColor', 'none red green blue yellow')
 
-color = type_color(
-    none="\033[0m",
-    red="\033[1;31m",
-    green="\033[1;32m",
-    blue="\033[1;34m",
-    yellow="\033[1;33m"
+color = TypeColor(
+    none='\033[0m',
+    red='\033[1;31m',
+    green='\033[1;32m',
+    blue='\033[1;34m',
+    yellow='\033[1;33m',
 )
 
 
 def msg_colored(msg, cor):
     """Colored text."""
-    return '{}{}{}'.format(cor, msg, color.none)
+    return f'{cor}{msg}{color.none}'
