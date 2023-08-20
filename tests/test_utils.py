30ae1de4c819c1d435ac1d7e15329f4b00cf18ba
Britodfbr <britodfbr@gmail.com>
2023-08-20 14:37:13 -0300

    style(linter): #0 Correções para cobertura PEP8 e outras indicações de linter aplicadas.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,16 @@
 """Test utils module."""
-from collections import namedtuple
-
 import pytest
+
 from incolume.py.model_2023_07_05 import utils
 
-__author__ = "@britodfbr"  # pragma: no cover
+__author__ = '@britodfbr'  # pragma: no cover
 
 
 class TestCaseUtils:
     """Test case."""
 
     def test_type(self):
-        assert type(utils.color) == utils.TypeColor
+        assert isinstance(utils.color, utils.TypeColor)
 
     @pytest.mark.parametrize(
         'entrance expected'.split(),
@@ -21,7 +20,7 @@ class TestCaseUtils:
             (utils.color.green, '\x1b[1;32m'),
             (utils.color.blue, '\x1b[1;34m'),
             (utils.color.yellow, '\x1b[1;33m'),
-        )
+        ),
     )
     def test_type_color(self, entrance, expected):
         """test it."""
@@ -34,7 +33,7 @@ class TestCaseUtils:
             (('xpto', utils.color.green), '\x1b[1;32mxpto\x1b[0m'),
             (('xpto', utils.color.red), '\x1b[1;31mxpto\x1b[0m'),
             (('xpto', utils.color.yellow), '\x1b[1;33mxpto\x1b[0m'),
-        )
+        ),
     )
     def test_msg_colored(self, entrance, expected):
         assert utils.msg_colored(*entrance) == expected
