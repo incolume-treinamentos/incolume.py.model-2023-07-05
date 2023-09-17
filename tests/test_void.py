0234b45e91baa0ad33d8cc43dd0979ca2c05131d
Britodfbr <britodfbr@gmail.com>
2023-09-17 17:36:43 -0300

    test(chore): #0 Incluído teste com id personalizado via pytest.param

--- tests/test_void.py
+++ tests/test_void.py
@@ -27,3 +27,20 @@ def test_none(capsys, entrance):
     out, err = capsys.readouterr()
     logging.debug('%s, %s', out, err)
     assert out.strip() == entrance
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("3+5", 8),
+        pytest.param("1+7", 8, marks=pytest.mark.basic),
+        pytest.param("2+4", 6, marks=pytest.mark.basic, id="basic_2+4"),
+        pytest.param(
+            "6*9", 42,
+            marks=[pytest.mark.basic, pytest.mark.xfail],
+            id="basic_6*9"
+        ),
+    ],
+)
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
